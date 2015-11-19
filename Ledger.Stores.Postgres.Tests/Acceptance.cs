f1fd25fe559b91770534828289bd64c9876e7574
andy <andy@andydote.co.uk>
2015-11-19 07:52:50 +0000

    packages: update to new ledger testing

---
+++ Ledger.Stores.Postgres.Tests/Acceptance.cs
@@ -0,0 +1,13 @@
+﻿using Ledger.Acceptance;
+using Npgsql;
+
+namespace Ledger.Stores.Postgres.Tests
+{
+	public class Acceptance : AcceptanceTests
+	{
+		public Acceptance()
+			: base(new PostgresEventStore(new NpgsqlConnection(PostgresTestBase.ConnectionString)))
+		{
+		}
+	}
+}
